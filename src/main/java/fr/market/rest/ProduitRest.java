f96fc4dda4e4af4a44e26f297b0ab173bd5dc803
SALAHmdk <135129411+SALAHmdk@users.noreply.github.com>
2023-07-04 00:17:30 +0200

    Add files via upload

--- src/main/java/fr/market/rest/ProduitRest.java
+++ src/main/java/fr/market/rest/ProduitRest.java
@@ -1,8 +1,13 @@
 package fr.market.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.market.dao.IProduitRepository;
@@ -16,7 +21,14 @@ public class ProduitRest {
 	@GetMapping("produit")
 	public Iterable <Produit>getAllProduit(){
 		return produitRepo.findAll();
-		
 	}
+	@PostMapping("produit")
+	public Produit saveProduit (@RequestBody Produit p) {
+	    return produitRepo.save(p);
+	}
+	@GetMapping("produit/{id}")
+    public Optional<Produit>getOneProduit(@PathVariable Long id){
+    	return produitRepo.findById(id);
+    }
 	
 }
